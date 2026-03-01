b1d6a520ce6b5a0f53aa01912f3f75d1d39aa32b
gozilla <wpdr5r@gmail.com>
2026-03-01 21:37:47 +0200

    add create_habit handler

--- app/routers/habits.py
+++ app/routers/habits.py
@@ -1,3 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-router = APIRouter()
+from app.dependency import get_habits_service
+from app.services.habits import HabitsService
+from app.schemas.habits import HabitsReadSchema, HabitsCreateSchema
+
+
+router = APIRouter(tags=["habits"])
+
+@router.post("/habit", response_model=HabitsReadSchema)
+def create_habit(habit: HabitsCreateSchema, habits_service: HabitsService = Depends(get_habits_service)):
+	return habits_service.create_habit(habit=habit)
