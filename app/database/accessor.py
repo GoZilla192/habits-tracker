0186703051581bffdd786a040f58c4296c82fb38
gozilla <wpdr5r@gmail.com>
2026-03-01 21:32:21 +0200

    set param expire_on_commit to False in session_factory

--- app/database/accessor.py
+++ app/database/accessor.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, Session
 from app.settings import Settings
 
 engine = create_engine(Settings().sqlite_db_url)
-session_factory = sessionmaker(bind=engine)
+session_factory = sessionmaker(bind=engine, expire_on_commit=False)
 
 def get_db_session() -> Session:
-	return session_factory
+	return session_factory()
