a1c753fab315b5e947f7144fdbbe8f59123f5291
gozilla <wpdr5r@gmail.com>
2026-03-04 16:25:54 +0200

    refactor sqlite_db_url to db_url add more fields to Settings

--- app/database/accessor.py
+++ app/database/accessor.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, Session
 
 from app.settings import Settings
 
-engine = create_engine(Settings().sqlite_db_url)
+engine = create_engine(Settings().db_url)
 session_factory = sessionmaker(bind=engine, expire_on_commit=False)
 
 def get_db_session() -> Session:
--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,26 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-	sqlite_path_to_db: str = "habit_tracker.db"
+	sqlite_path_to_db: str = "habit_tracker.db" # for test
+	
+	MYSQL_DRIVER: str = "mysql+mysqldb"
+	MYSQL_HOST: str
+	MYSQL_PORT: int
+	MYSQL_LOGIN: str
+	MYSQL_PASSWORD: str
+	MYSQL_DB: str
+	
+	model_config = SettingsConfigDict(env_file=".env")
+	
+	def get_sqlite_db_url_connection(self) -> str:
+		return f"sqlite:///{self.sqlite_path_to_db}"
+	
+	def get_mysql_db_url_connection(self) -> str:
+		return f"{self.MYSQL_DRIVER}://{self.MYSQL_LOGIN}:{self.MYSQL_PASSWORD}@{self.MYSQL_HOST}:{self.MYSQL_PORT}/{self.MYSQL_DB}"
 	
 	@property
-	def sqlite_db_url(self):
-		return f"sqlite:///{self.sqlite_path_to_db}"
+	def db_url(self) -> str:
+		return self.get_mysql_db_url_connection()
+	
+	
