84086dc81bad91bea57eb9a3e86d7ab24361cff3
gozilla <wpdr5r@gmail.com>
2026-03-08 11:44:30 +0200

    fix habits schemas

--- app/schemas/habits.py
+++ app/schemas/habits.py
@@ -4,18 +4,18 @@ from pydantic import BaseModel, ConfigDict
 
 class HabitsCreateSchema(BaseModel):
 	name: str
-	description: str | None
+	description: str | None = None
 
 
 class HabitsReadSchema(BaseModel):
 	id: int
 	name: str
-	description: str | None
+	description: str | None = None
 	created_at: datetime.date
 	
 	model_config = ConfigDict(from_attributes=True) # for convert orm model to pydantic model
 	
 class HabitsUpdateSchema(BaseModel):
 	name: str
-	description: str | None
+	description: str | None = None
 	
